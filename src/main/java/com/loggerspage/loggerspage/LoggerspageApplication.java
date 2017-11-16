1daa07b7d410ec2e0d42446a49b931ce32903700
caniven <caniven@canivens-MacBook-Pro.local>
2017-11-17 00:05:36 +0300

    Initial commit

---
+++ src/main/java/com/loggerspage/loggerspage/LoggerspageApplication.java
@@ -0,0 +1,12 @@
+package com.loggerspage.loggerspage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggerspageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggerspageApplication.class, args);
+	}
+}
